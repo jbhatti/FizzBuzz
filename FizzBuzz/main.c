f9ef9a34139efda1b678f312819d4f6f40f5c56a
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-06-26 14:28:18 -0400

    Initial Commit

---
+++ FizzBuzz/main.c
@@ -0,0 +1,41 @@
+//
+//  main.c
+//  FizzBuzz
+//
+//  Created by Jaison Bhatti on 2017-06-26.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int i;
+    
+    for (i = 1; i < 101; i++)
+    {
+        if (((i % 5) != 0) && ((i % 3) == 0)) {
+            printf("i = Fizz\n");
+        }
+        else
+        {
+            if (((i % 5) == 0) && ((i % 3) != 0)) {
+                printf("i = Buzz\n");
+            }
+            else
+            {
+                if (((i % 5) == 0) && ((i % 3) == 0)) {
+                    printf("i = FizzBuzz\n");
+                }
+                else
+                {
+                    printf("i = %d\n", i);
+                }
+            }
+        }
+    }
+        
+    return 0;
+}
+
+
